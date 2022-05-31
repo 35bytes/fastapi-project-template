626e4b7bedab77871ea9acb40552904357ef97d0
Karl Behrens <karlbehrensg@gmail.com>
2022-05-30 21:32:49 -0400

    Base project and test

---
+++ tests/e2e/test_api.py
@@ -0,0 +1,22 @@
+# System
+import sys
+import pathlib
+
+# Packages
+from fastapi.testclient import TestClient
+
+# Set root path
+PACKAGE_PARENT = pathlib.Path(__file__).parent.parent.parent
+sys.path.append(str(PACKAGE_PARENT))
+
+# Import other modules
+from src.entrypoints.main import app
+
+
+client = TestClient(app)
+
+
+def test_hello_worl():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
