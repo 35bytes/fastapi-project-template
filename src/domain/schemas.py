ed71f9ce58460e5ec7d1d7cffa270df326e273f1
Karl Behrens <karlbehrensg@gmail.com>
2022-05-31 00:14:33 -0400

    Define input schema

---
+++ src/domain/schemas.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    password2: str
--- src/entrypoints/main.py
+++ src/entrypoints/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
+from src.domain import schemas
+
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+@app.post("/")
+async def register_user(user: schemas.UserCreate):
     return {"message": "Hello World"}
