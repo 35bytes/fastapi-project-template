a3e09caf7dcb7621ead6b7ba6b85a26acbd9e444
Karl Behrens <karlbehrensg@gmail.com>
2022-06-05 19:29:14 -0400

    Create fixture to return TestClient app

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,11 @@ import os
 import pytest
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from fastapi.testclient import TestClient
 
 from config import Settings, get_settings, get_db_url
 from src.adapters.orm import Base
+from src.entrypoints.main import app
 
 
 @pytest.fixture(scope="module")
@@ -25,3 +27,9 @@ def create_db_in_memory(config_test):
     Base.metadata.create_all(engine)
     yield
     Base.metadata.drop_all(engine)
+
+
+@pytest.mark.usefixtures("create_db_in_memory")
+@pytest.fixture(scope="session")
+def client():
+    return TestClient(app)
