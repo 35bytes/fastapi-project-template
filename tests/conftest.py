45355b2a348dfa656e73cee9d1c65cd4e5b321c5
Karl Behrens <karlbehrensg@gmail.com>
2022-06-05 21:09:48 -0400

    Test endpoints with db for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,42 @@
-import os
 import pytest
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from fastapi.testclient import TestClient
 
-from config import Settings, get_settings, get_db_url
-from src.adapters.orm import Base
 from src.entrypoints.main import app
+from src.adapters.orm import Base, get_db
 
 
-@pytest.fixture
+SQLALCHEMY_TEST_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(
+    SQLALCHEMY_TEST_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@pytest.fixture(scope="session")
 def load_env_variables():
     load_dotenv()
 
 
-@pytest.fixture
-def client():
+@pytest.fixture(scope="session")
+def create_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture(scope="session")
+def client(create_db):
     return TestClient(app)
